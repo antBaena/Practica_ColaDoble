028319dc11e49224f4023b5506f92cb6891dda70
antBaena <antbaena@uma.es>
2024-03-21 12:33:46 +0100

    Test
    Co-authored-by: Antonio Blas Moral Sánchez <tbms18@users.noreply.github.com>

--- coladoble/src/test/java/deque/DoubleLinkedListTest.java
+++ coladoble/src/test/java/deque/DoubleLinkedListTest.java
@@ -4,6 +4,145 @@
 */
 package deque;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+@DisplayName("DoubleLinkedList")
 public class DoubleLinkedListTest {
+
+    @Nested
+    @DisplayName("cuando esta vacia")
+    class emptyDoubleQueue {
+
+        DoubleLinkedQueue<Integer> doubleList;
+
+        @BeforeEach
+        public void setUp() {
+            doubleList = new DoubleLinkedList<>();
+        }
+
+        @DisplayName("El método prepend debe añadir un elemento al principio de la cola")
+        @Test
+        public void Prepend_ValorValido_AnyadeElementoAlPrincipio() {
+            int valor = 1;
+
+            doubleList.prepend(valor);
+            int result = doubleList.first();
+
+            assertEquals(valor, result);
+        }
+
+        @DisplayName("El método prepend debe lanzar una excepcion si el valor es nulo")
+        @Test
+        void Prepend_ValorNull_LanzaExcepcion() {
+
+            assertThrows(DoubleLinkedQueueException.class, () -> doubleList.prepend(null));
+        }
+
+        @DisplayName("El método append debe añadir un elemento al final de la cola")
+        @Test
+        void Append_ValorValido_AnyadeElementoAlFinal() {
+            int valor = 1;
+
+            doubleList.append(valor);
+            int result = doubleList.last();
+
+            assertEquals(valor, result);
+        }
+
+        @DisplayName("El método append debe lanzar una excepcion si el valor es nulo")
+        @Test
+        void Append_ValorNull_LanzaExcepcion() {
+
+            assertThrows(DoubleLinkedQueueException.class, () -> doubleList.append(null));
+        }
+
+        @DisplayName("El método deleteFirst debe lanzar una excepcion si la lista esta vacia")
+        @Test
+        void DeleteFirst_ListaVacia_LanzaExcepcion() {
+            assertThrows(DoubleLinkedQueueException.class, () -> doubleList.deleteFirst());
+        }
+
+        @DisplayName("El método deleteLast debe lanzar una excepcion si la lista esta vacia")
+        @Test
+        void DeleteLast_ListaVacia_LanzaExcepcion() {
+            assertThrows(DoubleLinkedQueueException.class, () -> doubleList.deleteLast());
+        }
+    }
+
+    @Nested
+    @DisplayName("cuando tiene un elemento")
+    class oneElemetDoubleQueue {
+
+        DoubleLinkedQueue<Integer> doubleList;
+        int primerElemnto = 1;
+
+        @BeforeEach
+        public void setUp() {
+            doubleList = new DoubleLinkedList<>();
+            doubleList.append(primerElemnto);
+        }
+
+    }
+
+    @Nested
+    @DisplayName("cuando tiene varios elementos")
+    class normalDoubleQueue {
+
+        DoubleLinkedQueue<Integer> doubleList;
+
+        @BeforeEach
+        public void setUp() {
+            doubleList = new DoubleLinkedList<>();
+        }
+
+        @DisplayName("El método prepend debe añadir un elemento al principio de la cola")
+        @Test
+        public void Prepend_ValorValido_AnyadeElementoAlPrincipio() {
+            int valor = 1;
+
+            doubleList.prepend(valor);
+            int result = doubleList.first();
+
+            assertEquals(valor, result);
+        }
+
+        @DisplayName("El método prepend debe lanzar una excepcion si el valor es nulo")
+        @Test
+        void Prepend_ValorNull_LanzaExcepcion() {
+
+            assertThrows(DoubleLinkedQueueException.class, () -> doubleList.prepend(null));
+        }
+
+        @DisplayName("El método append debe añadir un elemento al final de la cola")
+        @Test
+        void Append_ValorValido_AnyadeElementoAlFinal() {
+            int valor = 1;
+
+            doubleList.append(valor);
+            int result = doubleList.last();
+
+            assertEquals(valor, result);
+        }
+
+        @DisplayName("El método append debe lanzar una excepcion si el valor es nulo")
+        @Test
+        void Append_ValorNull_LanzaExcepcion() {
+
+            assertThrows(DoubleLinkedQueueException.class, () -> doubleList.append(null));
+        }
+
+        @DisplayName("El método deleteFirst debe lanzar una excepcion si la lista esta vacia")
+        @Test
+        void DeleteFirst_ListaVacia_LanzaExcepcion() {
+            assertThrows(DoubleLinkedQueueException.class, () -> doubleList.deleteFirst());
+        }
+
+        @DisplayName("El método deleteLast debe lanzar una excepcion si la lista esta vacia")
+        @Test
+        void DeleteLast_ListaVacia_LanzaExcepcion() {
+            assertThrows(DoubleLinkedQueueException.class, () -> doubleList.deleteLast());
+        }
+    }
 
 }
--- coladoble/src/test/java/deque/NodeLinkedTest.java
+++ coladoble/src/test/java/deque/NodeLinkedTest.java
@@ -4,8 +4,18 @@
 */
 package deque;
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 
+/*
+    @DisplayName("El método <método> debe <devolver|lanzar> cuando <condición>")
+        @Test
+        void template() {
+            node.getItem().equals(item);
+    }
+ */
+
+@DisplayName("LinkedNode")
 public class NodeLinkedTest {
 
     LinkedNode<String> node;
@@ -21,10 +31,137 @@ public class NodeLinkedTest {
         node = new LinkedNode<String>(item, previous, next);
     }
 
-    @DisplayName("El método <método> debe <devolver|lanzar> cuando <condición>")
+    @DisplayName("El método getItem debe devolver el item")
     @Test
-    void template() {
-        node.getItem().equals(item);
+    void getItem_DevulveItem() {
+
+        String result = node.getItem();
+
+        assertEquals(item, result);
+
+    }
+
+    @DisplayName("El método getPrevious debe devolver null")
+    @Test
+    void getPrevious_DevuelveNull() {
+        LinkedNode<String> result = node.getPrevious();
+
+        assertNull(result);
+    }
+
+    @DisplayName("El método getNext debe devolver null")
+    @Test
+    void getNext_DevuelveNull() {
+        LinkedNode<String> result = node.getNext();
+
+        assertNull(result);
+    }
+
+    @DisplayName("El método setItem debe establecer el item")
+    @Test
+    void setItem_EstableceItem() {
+        String newItem = "newItem";
+
+        node.setItem(newItem);
+        String result = node.getItem();
+
+        assertEquals(newItem, result);
+    }
+
+    @DisplayName("El método setPrevious debe establecer el nodo anterior")
+    @Test
+    void setPrevious_EstableceNodoAnterior() {
+        LinkedNode<String> newPrevious = new LinkedNode<String>("previous", null, null);
+
+        node.setPrevious(newPrevious);
+
+        assertEquals(newPrevious, node.getPrevious());
+    }
+
+    @DisplayName("El método setNext debe establecer el nodo siguiente")
+    @Test
+    void setNext_EstableceNodoSiguiente() {
+        LinkedNode<String> newNext = new LinkedNode<String>("next", null, null);
+
+        node.setNext(newNext);
+
+        assertEquals(newNext, node.getNext());
+    }
+
+    @DisplayName("El método isFirstNode debe devolver false si el nodo no es el primero")
+    @Test
+    void isFirstNode_DevuelveFalse() {
+        LinkedNode<String> newNode = new LinkedNode<String>("new", null, null);
+        node.setPrevious(newNode);
+
+        boolean result = node.isFirstNode();
+
+        assertFalse(result);
+    }
+
+    @DisplayName("El método isFirstNode debe devolver true si el nodo es el primero")
+    @Test
+    void isFirstNode_DevuelveTrue() {
+        boolean result = node.isFirstNode();
+
+        assertTrue(result);
+    }
+
+    @DisplayName("El método isLastNode debe devolver false si el nodo no es el último")
+    @Test
+    void isLastNode_DevuelveFalse() {
+        LinkedNode<String> newNode = new LinkedNode<String>("new", null, null);
+        node.setNext(newNode);
+
+        boolean result = node.isLastNode();
+
+        assertFalse(result);
+    }
+
+    @DisplayName("El método isLastNode debe devolver true si el nodo es el último")
+    @Test
+    void isLastNode_DevuelveTrue() {
+        boolean result = node.isLastNode();
+
+        assertTrue(result);
+    }
+
+    @DisplayName("El método isNotATerminalNode debe devolver true si el nodo no es el primero ni el último")
+    @Test
+    void isNotATerminalNode_DevuelveTrue() {
+        LinkedNode<String> previous = new LinkedNode<String>("previous", null, null);
+        LinkedNode<String> next = new LinkedNode<String>("next", null, null);
+        LinkedNode<String> node = new LinkedNode<String>("test", previous, next);
+
+        boolean result = node.isNotATerminalNode();
+
+        assertTrue(result);
+    }
+
+    @DisplayName("El método isNotATerminalNode debe devolver false si el nodo es el primero")
+    @Test
+    void isNotATerminalNode_Primero_DevuelveFalse() {
+        LinkedNode<String> previous = null;
+        LinkedNode<String> next = new LinkedNode<String>("next", null, null);
+        LinkedNode<String> node = new LinkedNode<String>("test", previous, next);
+
+        boolean result = node.isNotATerminalNode();
+
+        assertFalse(result);
+
+    }
+
+    @DisplayName("El método isNotATerminalNode debe devolver false si el nodo es el último")
+    @Test
+    void isNotATerminalNode_Ultimo_DevuelveFalse() {
+        LinkedNode<String> previous = new LinkedNode<String>("previous", null, null);
+        LinkedNode<String> next = null;
+        LinkedNode<String> node = new LinkedNode<String>("test", previous, next);
+
+        boolean result = node.isNotATerminalNode();
+
+        assertFalse(result);
+
     }
 
 }
